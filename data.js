dcd69ec34a9d9c53ab59af5dd38072b1c99e1fe4
Dhiego Gomes <dhiego.jw@gmail.com>
2021-07-13 21:33:29 -0300

    :rocket: Envia a section 'services'

---
+++ data.js
@@ -0,0 +1,54 @@
+module.exports = {
+    services: [
+        {
+            service: 'Chave comum e tetra',
+            icon: 'chave-casa',
+            description: 'Cópias de chave comum e tetra'
+        },
+        {
+            service: 'Chave automotiva',
+            icon: 'chave-carro',
+            description: 'Com ferramentas originais apropriadas'
+        },
+        {
+            service: 'Abertura',
+            icon: 'abertura',
+            description: 'Residencial e automotiva'
+        },
+        {
+            service: 'Afiação',
+            icon: 'afiacao',
+            description: 'Alicates de cutícula, tesouras facas e ferramentas'
+        },
+        {
+            service: 'Recuperação de fechaduras',
+            icon: 'fechadura',
+            description: 'Residencial e automotiva'
+        },
+        {
+            service: 'Unificação de fechadura residencial',
+            icon: 'chave-casa-unificada',
+            description: ''
+        },
+        {
+            service: 'Unificação de chave automotiva',
+            icon: 'chave-carro-unificada',
+            description: 'Apenas modelos antigos. Verifique a disponibilidade'
+        },
+        {
+            service: 'Produtos à venda',
+            icon: 'produtos',
+            description: 'Apenas modelos antigos. Verifique a disponibilidade'
+        },
+        {
+            service: 'Retiramos e entregamos o seu material',
+            icon: 'delivery',
+            description: '(verifique a disponibilidade)'
+        },
+        {
+            service: 'Aceitamos cartões',
+            icon: 'cartao',
+            description: ''
+        },
+    ]
+}
